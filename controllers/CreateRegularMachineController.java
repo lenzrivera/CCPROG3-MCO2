f5abcdcc4feab226d590c18884b21a0b130c8ed5
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2023-07-22 15:15:08 +0800

    Implement CreateRegularMachineController
    Draft usage of associated classes (especially CreateRegularMachineView)
    were also added.

--- controllers/CreateRegularMachineController.java
+++ controllers/CreateRegularMachineController.java
@@ -1,8 +1,142 @@
 package controllers;
 
 import controllers.templates.CreateMachineController;
+import model.Item;
+import model.RegularVendingMachine;
+import model.Slot;
+import model.VendingMachine;
+import model.VendingMachineModel;
+import states.MainMenuState;
+import views.CreateRegularMachineView;
+import views.components.StockItemsPanel;
+import views.templates.CreateMachineView;
 
-// TODO
 public class CreateRegularMachineController extends CreateMachineController {
+    private CreateRegularMachineView view;
 
+    private RegularVendingMachine machine;
+    
+    public CreateRegularMachineController(
+        VendingMachineModel m, 
+        CreateRegularMachineView v
+    ) {
+        super(m);
+
+        view = v;
+        machine = null;
+
+        setConstants();
+
+        /* 1 - BASIC INFORMATION */
+
+        view.getBasicInfoPanel().setNextButtonListener((
+            String name, 
+            int slotCount, 
+            int slotCapacity
+        ) -> {
+            if (name.isBlank()) {
+                view.showErrorDialog("Please enter a valid name.");
+                return;
+            }
+
+            machine = new RegularVendingMachine(name, slotCount, slotCapacity);
+            view.getSetupPane().setActiveTab(1);
+
+            view.getStockItemsPanel().setMaxStock(slotCapacity);
+            view.getStockItemsPanel().setSlotCount(slotCount);
+            updateSlotTable(machine.getSlots());
+        });
+
+        /* 2 - ITEM STOCK */ 
+
+        view.getStockItemsPanel().setItemAddListener((
+            int slotNo,
+            String name,
+            double price,
+            double calories,
+            int stock
+        ) -> {
+            if (name.isBlank()) {
+                view.showErrorDialog("Please enter a valid item name.");
+                return;
+            }
+
+            for (Slot slot : machine.getSlots()) {
+                if (
+                    slot.getSampleItem() != null && 
+                    slot.getSampleItem().getName().equalsIgnoreCase(name)
+                ) {
+                    view.showErrorDialog("Please enter an unused item name.");
+                    return;
+                }
+            }
+
+            // In case an item is already in the slot.
+            machine.removeItem(slotNo);
+
+            machine.addItem(slotNo, name, price, calories);
+            machine.stockItem(slotNo, stock);
+            
+            updateSlotTable(machine.getSlots());
+        });
+
+        view.getStockItemsPanel().setItemRemoveListener((int slotNo) -> {
+            if (!machine.removeItem(slotNo)) {
+                view.showErrorDialog("Cannot remove a non-existent item.");
+                return;
+            }
+
+            updateSlotTable(machine.getSlots());
+        });
+
+        view.getStockItemsPanel().setNextButtonListener(() -> {
+            view.getSetupPane().setActiveTab(2);
+        });
+
+        view.getStockItemsPanel().setSlotSelectListener((
+            int selectedSlotNo
+        ) -> {
+            StockItemsPanel itemsPanel = view.getStockItemsPanel();
+
+            Slot selectedSlot = machine.getSlot(selectedSlotNo);
+            Item sampleItem = selectedSlot.getSampleItem();
+            
+            if (sampleItem == null) {
+                itemsPanel.setNameInputValue("");
+                itemsPanel.setCaloriesInputValue(0.0);
+                itemsPanel.setPriceInputValue(0.0);
+                itemsPanel.setStockInputValue(0);
+            } else {
+                itemsPanel.setNameInputValue(sampleItem.getName());
+                itemsPanel.setCaloriesInputValue(sampleItem.getCalories());
+                itemsPanel.setPriceInputValue(selectedSlot.getUnitPrice());
+                itemsPanel.setStockInputValue(selectedSlot.getStock());
+            }
+        });
+
+        /* 3 - CHANGE STOCK */
+
+        view.getStockChangePanel().setAddDenominationListener((
+            double denom, 
+            int quantity
+        ) -> {
+            machine.stockChange(denom, quantity);
+            updateDenominationTable(machine.getChangeStock());
+        });
+
+        view.getStockChangePanel().setNextButtonListener(() -> {
+            model.setVendingMachine(machine);
+            changeState(new MainMenuState());
+        });
+    }
+
+    @Override
+    protected VendingMachine getMachine() {
+        return machine;
+    }
+
+    @Override
+    protected CreateMachineView getView() {
+        return view;
+    }
 }
