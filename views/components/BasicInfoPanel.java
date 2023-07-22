b47a8ea996fd8d359e5f311e3be1df9dc789c9e5
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2023-07-22 15:22:02 +0800

    Add BasicInfoPanel
    BasicInfoPanel represents the interface for entering the basic common
    information required for any type of vending machine.

---
+++ views/components/BasicInfoPanel.java
@@ -0,0 +1,105 @@
+package views.components;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
+import javax.swing.SpinnerNumberModel;
+
+public class BasicInfoPanel extends JPanel {
+    private JLabel heading;
+    private JLabel nameLabel;
+    private JTextField nameInput;
+    private JLabel slotCountLabel;
+    private JSpinner slotCountInput;
+    private JLabel slotCapacityLabel;
+    private JSpinner slotCapacityInput;
+    private JButton nextButton;
+
+    public interface NextButtonListener {
+        public void run(String name, int slotCount, int slotCapacity);
+    }
+
+    public BasicInfoPanel() {
+        super(new GridBagLayout());
+
+        GridBagConstraints gbc = new GridBagConstraints();  
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(5, 5, 5, 5);
+
+        gbc.gridy = 0;
+
+        heading = new JLabel("<html><u>Enter Basic Information</u></html>");
+        gbc.gridwidth = 2;        
+        add(heading, gbc);
+
+        gbc.gridwidth = 1;
+        gbc.gridy = 1;
+
+        nameLabel = new JLabel("Name: ");
+        gbc.gridx = 0;
+        add(nameLabel, gbc);
+
+        nameInput = new JTextField(15);
+        gbc.gridx = 1;
+        add(nameInput, gbc);
+
+        gbc.gridy = 2;
+
+        slotCountLabel = new JLabel("Number of slots: ");
+        gbc.gridx = 0;
+        add(slotCountLabel, gbc);
+
+        slotCountInput = new JSpinner();
+        gbc.gridx = 1;
+        add(slotCountInput, gbc);
+
+        gbc.gridy = 3;
+
+        slotCapacityLabel = new JLabel("Slot capacity: ");
+        gbc.gridx = 0;
+        add(slotCapacityLabel, gbc);
+
+        slotCapacityInput = new JSpinner();
+        gbc.gridx = 1;
+        add(slotCapacityInput, gbc);
+        
+        gbc.gridwidth = 2;
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+
+        nextButton = new JButton("Next");
+        add(nextButton, gbc);
+    }
+
+    public void setMinSlotCount(int minSlotCount) {
+        SpinnerNumberModel model = new SpinnerNumberModel();
+        model.setMinimum(minSlotCount);  
+        model.setValue(minSlotCount);
+
+        slotCountInput.setModel(model);
+    }
+
+    public void setMinSlotCapacity(int minSlotCapacity) {
+        SpinnerNumberModel model = new SpinnerNumberModel();
+        model.setMinimum(minSlotCapacity);  
+        model.setValue(minSlotCapacity);
+
+        slotCapacityInput.setModel(model);
+    }
+
+    public void setNextButtonListener(NextButtonListener listener) {
+        nextButton.addActionListener(e -> {
+            listener.run(
+                nameInput.getText(), 
+                (int) slotCountInput.getValue(), 
+                (int) slotCapacityInput.getValue()
+            );
+        });
+    }
+}
