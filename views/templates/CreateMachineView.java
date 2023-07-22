71a8517b2599d2a8ef39f145c18937ac27a54958
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2023-07-22 15:18:32 +0800

    Implement CreateMachineView abstract base class
    CreateMachineView contains common functionality for the regular
    machine creation view and the special machine creation view.
    
    Added draft code for the view components used in CreateMachineView as
    well.

--- views/templates/CreateMachineView.java
+++ views/templates/CreateMachineView.java
@@ -1,6 +1,61 @@
 package views.templates;
 
-// TODO
-public class CreateMachineView {
-    
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.GridLayout;
+
+import javax.swing.JPanel;
+
+import util.View;
+import views.components.BasicInfoPanel;
+import views.components.SectionContainer;
+import views.components.SetupPane;
+import views.components.StockChangePanel;
+import views.components.StockItemsPanel;
+
+public abstract class CreateMachineView extends View {
+    protected SectionContainer mainContainer;
+
+    protected JPanel setupPaneContainer;
+    protected SetupPane setupPane;
+
+    protected BasicInfoPanel basicInfoPanel;
+    protected StockChangePanel stockChangePanel;
+
+    public CreateMachineView(String heading) {
+        mainContainer = new SectionContainer(heading);
+
+        setupPaneContainer = new JPanel();
+        mainContainer.setBody(setupPaneContainer);
+        
+        // Remove the indentation given to the heading by default.
+        setupPaneContainer.setAlignmentX(Component.LEFT_ALIGNMENT); 
+        // Make the tabbed pane take all the window space.
+        setupPaneContainer.setLayout(new GridLayout());
+
+        setupPane = new SetupPane();
+        setupPaneContainer.add(setupPane);
+
+        basicInfoPanel = new BasicInfoPanel();
+        stockChangePanel = new StockChangePanel();
+    }
+
+    @Override
+    public Container getContainer() {
+        return mainContainer;
+    }
+
+    public BasicInfoPanel getBasicInfoPanel() {
+        return basicInfoPanel;
+    }
+
+    public SetupPane getSetupPane() {
+        return setupPane;
+    }
+
+    public abstract StockItemsPanel getStockItemsPanel();
+
+    public StockChangePanel getStockChangePanel() {
+        return stockChangePanel;
+    }
 }
