1d6d2c53008a5639ef8399d0d58589c16c2e952d
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2023-07-25 15:17:08 +0800

    Add javadoc for TestMachineMenuController

--- controllers/TestMachineMenuController.java
+++ controllers/TestMachineMenuController.java
@@ -8,10 +8,29 @@ import states.MainMenuState;
 import util.Controller;
 import views.TestMachineMenuView;
 
+/**
+ * This controller class handles user interaction in the test machine menu,
+ * allowing the user to test either the vending or maintenance features of a 
+ * vending machine.
+ */
 public class TestMachineMenuController extends Controller {
+    /**
+     * The VendingMachineModel for the whole simulator.
+     */
     private VendingMachineModel model;
+
+    /**
+     * The view associated with this controller, particularly 
+     * TestMachineMenuView.
+     */
     private TestMachineMenuView view; 
 
+    /**
+     * Constructs a TestMachineMenuController object with its associated model 
+     * and view.
+     * @param m the VendingMachineModel associated with this controller
+     * @param v the TestMachineMenuView associated with this controller
+     */
     public TestMachineMenuController(VendingMachineModel m, TestMachineMenuView v) {
         model = m;
         view = v;
@@ -26,7 +45,7 @@ public class TestMachineMenuController extends Controller {
             } else {
                 // changeState(new TestSpecialVendingState());
             }
-    });
+        });
 
         view.setTestMaintenanceButtonListener(e -> {
             if (model.getVendingMachine() instanceof RegularVendingMachine) {
