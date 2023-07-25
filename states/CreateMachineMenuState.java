575ff874087cb874c795da634b0743c3b4e9a8b5
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2023-07-25 10:13:50 +0800

    Add javadoc for CreateMachineMenuState

--- states/CreateMachineMenuState.java
+++ states/CreateMachineMenuState.java
@@ -7,28 +7,59 @@ import util.State;
 import util.View;
 import views.CreateMachineMenuView;
 
+/**
+ * Represents the state where the user can create a new regular or special
+ * vending machine.
+ */
 public class CreateMachineMenuState extends State {
+    /**
+     * The view associated with this state, particularly CreateMachineMenuView.
+     */
     private CreateMachineMenuView view;
+
+    /**
+     * The controller associated with this state, particularly
+     * CreateMachineMenuController.
+     */
     private CreateMachineMenuController controller;
-    
+
+    /**
+     * Constructs a new CreateMachineMenuState object. The view and controller
+     * is initially set to null and initialized only when initialize() is 
+     * called.
+     */
     public CreateMachineMenuState() {
         view = null;
         controller = null;
     }
 
+    /**
+     * Retrieves the controller associated with this state.
+     * @return the controller associated with this state.
+     */
     @Override
     protected Controller getController() {
         return controller;
     }
 
+    /**
+     * Retrieves the view associated with this state.
+     * @return the view associated with this state.
+     */
     @Override
     public View getView() {
         return view;
     }
 
+    /**
+     * Passes the main model to the state and initializes its view and 
+     * controller using it. Relegating view and controller initialization here
+     * allows States to be passed around without initializing them immediately.
+     * @param model the VendingMachineModel to be associated with this state
+     */
     @Override
     public void initialize(VendingMachineModel model) {
         view = new CreateMachineMenuView();
         controller = new CreateMachineMenuController(view);
-    }   
+    }
 }
