1d8c94846b3c1ac8892c991fe2b337daf9564fbc
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2023-07-25 09:52:52 +0800

    Add javadoc for StateChangeListener

--- util/StateChangeListener.java
+++ util/StateChangeListener.java
@@ -2,6 +2,14 @@ package util;
 
 import java.util.EventListener;
 
+/**
+ * This interface represents a listener to be invoked when a State calls for
+ * for a state change.
+ */
 public interface StateChangeListener extends EventListener {
+    /**
+     * This method is called when a state change event occurs.
+     * @param state the new State that the program will transition to
+     */
     public void onChange(State state);
 }
