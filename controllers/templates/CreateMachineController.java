f6a1a5700e89ed1ed48f54026bd14531db903066
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2023-07-25 11:50:09 +0800

    Add javadoc for CreateMachineController

--- controllers/templates/CreateMachineController.java
+++ controllers/templates/CreateMachineController.java
@@ -12,14 +12,37 @@ import model.VendingMachineModel;
 import util.Controller;
 import views.templates.CreateMachineView;
 
+/**
+ * This abstract class contains the common functionality needed by the 
+ * controllers handling vending machine creation. The types of its view and
+ * internal vending machine instance are parametrized to minimize extra code
+ * for casting their types.
+ */
 public abstract class CreateMachineController<
     T extends CreateMachineView<?>, 
     U extends VendingMachine<?>> extends Controller
 {
+    /**
+     * The VendingMachineModel for the whole simulator.
+     */
     protected VendingMachineModel model;
+
+    /**
+     * The CreateMachineView associated with this controller.
+     */
     protected T view;
+
+    /**
+     * The vending machine being created by this controller.
+     */
     protected U machine;
 
+    /**
+     * Constructs a new CreateMachineController object, correspondingly
+     * initializing its model, view, and machine to be created.
+     * @param m the VendingMachineModel associated with this controller.
+     * @param v the specific CreateMachineView associated with this controller.
+     */
     public CreateMachineController(VendingMachineModel m, T v) {
         model = m;
         view = v;
@@ -28,6 +51,10 @@ public abstract class CreateMachineController<
         setConstants();
     }
 
+    /**
+     * Sets the constants and values for the view which limit the user's 
+     * actions to the required specifications.
+     */
     protected void setConstants() {
         view.getBasicInfoPanel()
             .setMinSlotCount(VendingMachine.MIN_SLOT_COUNT);
@@ -38,6 +65,11 @@ public abstract class CreateMachineController<
             Arrays.stream(DenominationMap.VALID_DENOMINATIONS).boxed().toList());
     }
 
+    /**
+     * Updates the stock change panel's denomination table with the provided 
+     * DenominationMap.
+     * @param denomMap the DenominationMap containing the denominations and quantities.
+     */
     protected void updateDenominationTable(DenominationMap denomMap) {
         Map<Double, Integer> rawMap = denomMap.getDenominations();
 
@@ -52,6 +84,12 @@ public abstract class CreateMachineController<
         }
     }
 
+    /**
+     * Updates the stock items panel's slot table with the provided list of slots.
+     * Each row in the table represents a slot with its slot number and the item name 
+     * (or [empty] for empty slots).
+     * @param slots the list of slots to be displayed in the stock items panel.
+     */
     protected void updateSlotTable(List<? extends Slot> slots) {
         for (int i = 0; i < slots.size(); i++) {
             String name = (slots.get(i).getSampleItem() == null) 
