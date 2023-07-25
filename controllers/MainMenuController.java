65604ff835e124470ffe0683028297e72cc02a73
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2023-07-25 15:14:11 +0800

    Add javadoc for MainMenuController

--- controllers/MainMenuController.java
+++ controllers/MainMenuController.java
@@ -6,10 +6,28 @@ import states.TestMachineMenuState;
 import util.Controller;
 import views.MainMenuView;
 
+/**
+ * This controller class is responsible for handling user interactions in
+ * the main menu, allowing them to either create or test a vending machine.
+ */
 public class MainMenuController extends Controller {
+    /**
+     * The VendingMachineModel for the whole simulator.
+     */
     private VendingMachineModel model;
+
+    /**
+     * The view associated with this controller, particularly 
+     * MainMenuView.
+     */
     private MainMenuView view;
     
+    /**
+     * Constructs a MainMenuController object with its associated model and 
+     * view.
+     * @param m the VendingMachineModel associated with this controller
+     * @param v the MainMenuView associated with this controller
+     */
     public MainMenuController(VendingMachineModel m, MainMenuView v) {
         model = m;
         view = v;
