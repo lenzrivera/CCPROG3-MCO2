c00763e71b61d9da7191d2ace2056c24ba158e1b
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2023-07-25 10:04:44 +0800

    Add javadoc for View

--- util/View.java
+++ util/View.java
@@ -4,12 +4,30 @@ import java.awt.Container;
 
 import javax.swing.JOptionPane;
 
+/**
+ * This abstract class represents an MVC view that contains the contents
+ * to graphically display to the user. 
+ */
 public abstract class View {
+    /**
+     * Constructs a new View object.
+     */
     public View() {
     }
 
+    /**
+     * Retrieves the main container of the view. The container holds all the
+     * visual components for the specific view.
+     * @return the Container instance representing the main container of the 
+     * view.
+     */
     public abstract Container getContainer();
 
+    /**
+     * Displays an error dialog with the specified message. The dialog is
+     * associated with the main container of the view. 
+     * @param message the error message to be displayed in the dialog.
+     */
     public void showErrorDialog(String message) {
         JOptionPane.showMessageDialog(
             getContainer(), message, "Error!", JOptionPane.ERROR_MESSAGE);
