f95717dea0ca76d048ac027f93168a6f71838200
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2023-08-01 17:16:54 +0800

    Add javadoc for CreateRegularMachineView

--- views/CreateRegularMachineView.java
+++ views/CreateRegularMachineView.java
@@ -4,7 +4,15 @@ import views.components.SetupBody;
 import views.components.SetupRegularItemsPanel;
 import views.templates.CreateMachineView;
 
+/**
+ * This class represents the view for the regular vending machine creation 
+ * screen. It extends the CreateMachineView class with a specific type of 
+ * setup items panel for regular vending machines.
+ */
 public class CreateRegularMachineView extends CreateMachineView<SetupRegularItemsPanel> {
+    /**
+     * Constructs a new CreateRegularMachineView.
+     */
     public CreateRegularMachineView() {
         super("Create a Regular Vending Machine");
         
