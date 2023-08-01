5091978de91e56425a2c0d72aec3fe1eb1b74eef
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2023-08-01 18:35:13 +0800

    Add javadoc for ItemQtySelector

--- views/components/ItemQtySelector.java
+++ views/components/ItemQtySelector.java
@@ -8,10 +8,26 @@ import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
 
+/**
+ * This class represents a componenting for selecting the quantity of an item.
+ * It consists of a label to display the item name and a spinner to input the 
+ * quantity.
+ */
 public class ItemQtySelector extends JPanel {
+    /**
+     * The name of the item whose quantity is being selected
+     */
     private JLabel itemName;
+
+    /**
+     * The spinner that allows a quantity to be entered
+     */
     private JSpinner spinner;
 
+    /**
+     * Creates an ItemQtySelector with an empty item name and initial quantity
+     * set to zero.
+     */
     public ItemQtySelector() {
         super(new GridLayout(1, 2));
 
@@ -26,23 +42,42 @@ public class ItemQtySelector extends JPanel {
         add(spinner);
 
         setMaximumSize(new Dimension(
-            getMaximumSize().width,getMinimumSize().height));
+            getMaximumSize().width, getMinimumSize().height));
     }
 
+    /**
+     * Returns the name of the item associated with this selector.
+     * @return the name of the item.
+     */
     public String getItemName() {
         return itemName.getText();
     }
 
+    /**
+     * Sets the name of the item associated with this selector.
+     * @param name the name of the item.
+     */
     public void setItemName(String name) {
         itemName.setText(name);
     }
 
+    /**
+     * Returns the selected quantity of the item.
+     * @return the selected quantity.
+     */
     public int getQuantity() {
         return (int) spinner.getValue();
     }
 
+    /**
+     * Sets the quantity of the item. The quantity must be non-negative (>= 0).
+     * @param quantity the quantity to set.
+     * @throws IllegalArgumentException if the quantity is negative.
+     */
     public void setQuantity(int quantity) {
-        assert quantity >= 0;
+        if (quantity < 0) {
+            throw new IllegalArgumentException("Quantity must be non-negative");
+        }
         spinner.setValue(quantity);
     }
 }
