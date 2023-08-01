5134e4696475b0c9b679227a763902d758a190ca
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2023-08-01 17:31:29 +0800

    Add javadoc for TestMaintenanceView

--- views/TestMaintenanceView.java
+++ views/TestMaintenanceView.java
@@ -11,15 +11,40 @@ import views.components.SectionContainer;
 import views.components.ManageItemsPanel;
 import views.components.SummaryViewPanel;
 
+/**
+ * This class represents the view for testing the maintenance features of a
+ * vending machine. It provides options to view the transaction summary, 
+ * manage items, and manage money.
+ */
 public class TestMaintenanceView extends View {
+    /**
+     * The section container for the GUI components.
+     */
     protected SectionContainer mainContainer;
 
+    /**
+     * The JTabbedPane to hold the different maintenance feature options.
+     */
     protected JTabbedPane optionTabs;
 
+    /**
+     * The panel to view the summary of the vending machine transactions.
+     */
     protected SummaryViewPanel summaryViewPanel;
+
+    /**
+     * The panel to manage the items in the vending machine.
+     */
     protected ManageItemsPanel manageItemsPanel;
+
+    /**
+     * The panel to manage the money in the vending machine.
+     */
     protected ManageMoneyPanel manageMoneyPanel;
 
+    /**
+     * Constructs a new TestMaintenanceView with its associated GUI components.
+     */
     public TestMaintenanceView() {
         mainContainer = new SectionContainer("Test Maintenance Features");
 
@@ -41,18 +66,37 @@ public class TestMaintenanceView extends View {
         return mainContainer;
     }
 
+    /**
+     * Returns the SummaryViewPanel instance used to view the transaction
+     * summary.
+     * @return the SummaryViewPanel instance.
+     */
     public SummaryViewPanel getSummaryViewPanel() {
         return summaryViewPanel;
     }
 
+    /**
+     * Returns the ManageItemsPanel instance used to manage the items
+     * in the machine.
+     * @return the SummaryViewPanel instance.
+     */
     public ManageItemsPanel getManageItemsPanel() {
         return manageItemsPanel;
     }
 
+    /**
+     * Returns the ManageMoneyPanel instance used to manage the money
+     * in the machine.
+     * @return the SummaryViewPanel instance.
+     */
     public ManageMoneyPanel getManageMoneyPanel() {
         return manageMoneyPanel;
     }
 
+    /**
+     * Sets the listener for the exit button.
+     * @param listener the listener for the exit button
+     */
     public void setExitButtonListener(ActionListener listener) {
         mainContainer.setExitButtonListener(listener);
     }
