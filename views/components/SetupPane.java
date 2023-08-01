bab03bfb5e37cb6fcd1744b4c00f0fc1129954b1
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2023-08-01 19:41:34 +0800

    Add javadoc for SetupPane

--- views/components/SetupPane.java
+++ views/components/SetupPane.java
@@ -4,10 +4,25 @@ import java.awt.Component;
 
 import javax.swing.JTabbedPane;
 
+/**
+ * This class represents a custom JTabbedPane for a setup process. It 
+ * automatically disables all tabs except the first one when they are added.
+ * It provides a method to set the active tab and enable it while disabling 
+ * other tabs.
+ */
 public class SetupPane extends JTabbedPane {
+    /**
+     * Creates a new instance of the SetupPane.
+     */
     public SetupPane() {
     }
     
+    /**
+     * Adds a new tab to the setup pane with the given title and component.
+     * It automatically disables all tabs except the first one when they are added.
+     * @param title the title of the tab.
+     * @param component the component to be displayed in the tab.
+     */
     @Override
     public void addTab(String title, Component component) {
         super.addTab(title, component);
@@ -18,6 +33,10 @@ public class SetupPane extends JTabbedPane {
         }
     }
 
+    /**
+     * Sets the active tab and enables it while disabling other tabs.
+     * @param tabIndex the index of the tab to be set as active.
+     */
     public void setActiveTab(int tabIndex) {
         for (int i = 0; i < getTabCount(); i++) {
             setEnabledAt(i, false);
