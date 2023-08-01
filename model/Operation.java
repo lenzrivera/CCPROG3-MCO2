a8c071974b86880ec078a381c9d04305d56c873e
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2023-08-01 12:09:02 +0800

    Add javadoc for Operation

--- model/Operation.java
+++ model/Operation.java
@@ -1,12 +1,41 @@
 package model;
 
+/**
+ * The Operation enum represents different food preparation operations 
+ * in the context of a vending machine.
+ */
 public enum Operation {
+    /**
+     * The item should be cooked.
+     */
     COOK,
+
+    /**
+     * The item should be heated.
+     */
     HEAT,
+
+    /**
+     * The item should be "prepared" in a general sense.
+     */
     PREPARE,
+    
+    /**
+     * The item should be spread.
+     */
     SPREAD,
+
+    /**
+     * The item should be topped.
+     */
     TOP;
 
+    /**
+     * Get the process message for the operation and the given item name.
+     * @param itemName The name of the item being prepared.
+     * @return the process message corresponding to the operation and the item
+     * name.
+     */
     public String getProcessMessage(String itemName) {
         switch (this) {
             case COOK:
