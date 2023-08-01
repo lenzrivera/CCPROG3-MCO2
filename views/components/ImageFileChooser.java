7fdc9c3c662555b6b9762c5d2a41be3a2e84bb6c
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2023-08-01 18:32:34 +0800

    Add javadoc for ItemFileChooser

--- views/components/ImageFileChooser.java
+++ views/components/ImageFileChooser.java
@@ -11,14 +11,39 @@ import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
+/**
+ * This class wraps around a JFileChooser to allow for image file selections. 
+ */
 public class ImageFileChooser extends JPanel {
+    /**
+     * The full path of the selected image
+     */
     private String fileFullPath;
-    
+
+    /**
+     * The main label of the file chooser
+     */
     private JLabel mainLabel;
-    private JLabel filepathLabel;
+
+    /**
+     * Tha main label indicating the name of the selected file
+     */
+    private JLabel filename;
+
+    /**
+     * The button that pops up the file chooser
+     */
     private JButton chooseButton;
+
+    /**
+     * The file chooser dialog
+     */
     private JFileChooser fileChooser;
-    
+
+    /**
+     * Constructs an ImageFileChooser instance with the specified main label.
+     * @param mainLabel the main label to be displayed on the file chooser.
+     */
     public ImageFileChooser(String mainLabel) {
         super(new GridBagLayout());
 
@@ -29,22 +54,21 @@ public class ImageFileChooser extends JPanel {
         gbc.gridx = 0;
         add(this.mainLabel, gbc);
 
-        filepathLabel = new JLabel("");
-        filepathLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        filename = new JLabel("");
+        filename.setHorizontalAlignment(SwingConstants.CENTER);
         gbc.gridx = 1;
         gbc.weightx = 1.0;
-        add(filepathLabel, gbc);
+        add(filename, gbc);
 
         chooseButton = new JButton("Select");
         gbc.gridx = 2;
         gbc.weightx = 0.0;
         add(chooseButton, gbc);
 
-        // TODO: actually disallow non-image files as this can be circumvented
-        FileNameExtensionFilter filter = 
+        FileNameExtensionFilter filter =
             new FileNameExtensionFilter("Image Files", "jpg", "png");
 
-        fileChooser = new JFileChooser(); 
+        fileChooser = new JFileChooser();
         fileChooser.setFileFilter(filter);
 
         chooseButton.addActionListener(e -> {
@@ -55,19 +79,29 @@ public class ImageFileChooser extends JPanel {
                 setFilePath(filepath);
             }
         });
-    } 
+    }
 
+    /**
+     * Returns the full path of the selected image file.
+     * @return the full path of the selected image file.
+     */
     public String getFilePath() {
         return fileFullPath;
     }
 
+    /**
+     * Sets the full path of the selected image file and updates the displayed 
+     * file name label accordingly. If the provided value is null or empty, the 
+     * file name label will display "[none]".
+     * @param value the full path of the selected image file.
+     */
     public void setFilePath(String value) {
         fileFullPath = value;
 
         if (value == null || value.isBlank()) {
-            filepathLabel.setText("[none]");
+            filename.setText("[none]");
         } else {
-            filepathLabel.setText(Paths.get(value).getFileName().toString());
+            filename.setText(Paths.get(value).getFileName().toString());
         }
     }
 }
